75a7c454cd9d8749444fbc04341c636f5d9f28cf
Meithan West <meithan@gmail.com>
2023-08-09 14:42:54 -0600

    Add Julia+LoopVec benchmark, fix errorbars

--- plot_benchmarks.py
+++ plot_benchmarks.py
@@ -26,14 +26,16 @@ for l in langs:
   mean = sum(data[l])/len(data[l])
   minv = min(data[l])
   maxv = max(data[l])
-  results.append([l, 0, mean, minv, maxv])
+  results.append([l, 0, mean, minv, maxv, 0, 0])
 results.sort(key=lambda x: x[2])
-ref_idx = 1
+ref_idx = [x[0] for x in results].index("C/C++")
 ref_value = results[ref_idx][2]
 for i in range(len(results)):
   results[i][1] = results[i][2] / ref_value
+  results[i][5] = results[i][3] / ref_value
+  results[i][6] = results[i][4] / ref_value
 
-colors = ["C0", "C1", "C2", "C4", "C5", "C6", "C7", "C8", "C9"]
+colors = ["C0", "C3", "C1", "C2", "C4", "C5", "C6", "C7", "C8", "C9"]
 title = "Euler1D, Sod shock tube, NX=5000, Lax-Friedrichs solver"
 
 # ----------------------------
@@ -44,16 +46,17 @@ results2 = [r for r in results if r not in results1]
 len1 = len(results1)
 len2 = len(results2)
 
-fig, axs = plt.subplots(1, 2, figsize=(9,5), gridspec_kw={'width_ratios': [len1, len2]})
+fig, axs = plt.subplots(1, 2, figsize=(10,5), gridspec_kw={'width_ratios': [len1, len2]})
 
 for subnum, _results in enumerate([results1, results2]):
 
   plt.sca(axs[subnum])
 
   xs = range(len(_results))
-  labels, ratios, means, mins, maxs = zip(*_results)
+  labels, ratios, means, minvs, maxvs, minrs, maxrs = zip(*_results)
   labels = [x.replace("+numpy", "\n+numpy") for x in labels]
-  yerrs = np.abs(np.array([mins, maxs]) - means)
+  labels = [x.replace("Julia+", "Julia\n+") for x in labels]
+  yerrs = np.abs(np.array([minrs, maxrs]) - ratios)
   plt.bar(xs, ratios, yerr=yerrs, capsize=3, width=0.5, tick_label=labels, log=False, color=colors[subnum*len1:(subnum+1)*len1])
   for i in xs:
     x = xs[i]
@@ -97,10 +100,10 @@ else:
 plt.figure(figsize=(9,5))
 
 xs = range(len(results))
-labels, ratios, means, mins, maxs = zip(*results)
-print(labels)
+labels, ratios, means, minvs, maxvs, minrs, maxrs = zip(*results)
 labels = [x.replace("+numpy", "\n+numpy") for x in labels]
-yerrs = np.abs(np.array([mins, maxs]) - means)
+labels = [x.replace("Julia+", "Julia\n+") for x in labels]
+yerrs = np.abs(np.array([minrs, maxrs]) - ratios)
 plt.bar(xs, ratios, yerr=yerrs, capsize=3, width=0.5, tick_label=labels, log=False, color=colors[:len(results)])
 for i in xs:
   x = xs[i]
