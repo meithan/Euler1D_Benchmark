16a23fbfea0e461a8a9c4b4008dd96d3f1d9f8de
Meithan West <meithan@gmail.com>
2023-08-10 13:44:41 -0600

    Add comment for Julia+LoopVec

--- run_benchmarks.py
+++ run_benchmarks.py
@@ -27,14 +27,15 @@ def run_benchmark(setup_cmds, test_cmd, num_runs):
 # The only output of the test must be the execution time
 benchmarks = [
   ("C/C++", ["rm -f Euler1D_C", "g++ -O3 Euler1D.cpp -o Euler1D_C"], "./Euler1D_C"),
-  # ("Fortran", ["rm -f Euler1D_F", "gfortran -O3 Euler1D.f90 -o Euler1D_F"], "./Euler1D_F"),
-  # ("Java", ["rm -f Euler1D.class", "javac Euler1D.java"], "java Euler1D"),
-  # ("Python3.8", [], "python3.8 Euler1D.py"),
-  # ("Python3.8+Numpy", [], "python3.8 Euler1D_numpy.py"),
-  # ("Python3.11", [], "python3.11 Euler1D.py"),
-  # ("Python3.11+Numpy", [], "python3.11 Euler1D_numpy.py"),
-  # ("Julia", [], "julia Euler1D.jl"),
-  # ("Rust", ["rustc -C opt-level=3 -o Euler1D_Rust Euler1D.rs"], "./Euler1D_Rust"),
+  ("Fortran", ["rm -f Euler1D_F", "gfortran -O3 Euler1D.f90 -o Euler1D_F"], "./Euler1D_F"),
+  ("Java", ["rm -f Euler1D.class", "javac Euler1D.java"], "java Euler1D"),
+  ("Python3.8", [], "python3.8 Euler1D.py"),
+  ("Python3.8+Numpy", [], "python3.8 Euler1D_numpy.py"),
+  ("Python3.11", [], "python3.11 Euler1D.py"),
+  ("Python3.11+Numpy", [], "python3.11 Euler1D_numpy.py"),
+  ("Julia", [], "julia Euler1D.jl"),
+  # Julia+LoopVec run manually in Julia REPL
+  ("Rust", ["rustc -C opt-level=3 -o Euler1D_Rust Euler1D.rs"], "./Euler1D_Rust"),
 ]
 
 num_runs = 10
